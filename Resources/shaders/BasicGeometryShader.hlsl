0079e274cedb7200726d70c62ae6f1fdd36abaf7
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-10-31 14:33:12 +0900

    でーきた

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,29 +1,25 @@
 #include "BasicShaderHeader.hlsli"
 
-[maxvertexcount(6)]
+[maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION, 
-	inout TriangleStream< GSOutput > output
+	point VSOutput input[1] : SV_POSITION,
+	inout TriangleStream<GSOutput> output
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
-        GSOutput element; //出力用頂点データ
-        element.svpos = input[i].svpos; //頂点座標をコピー
-        element.normal = input[i].normal; //法線をコピー
-        element.uv = input[i].uv; //UVをコピー
-		//頂点を1つ出力(出力リストに追加)
-        output.Append(element);
-    }
-    output.RestartStrip();
-    
-    for (uint i = 0; i < 3; i++)
-    {
-        GSOutput element; //出力用頂点データ
-        element.svpos = input[i].svpos + float4(10.0f,0.0f,0.0f,0.0f); //頂点座標をコピー
-        element.normal = input[i].normal; //法線をコピー
-        element.uv = input[i].uv * 5.0f; //UVをコピー
-		//頂点を1つ出力(出力リストに追加)
-        output.Append(element);
-    }
+    GSOutput element; //出力用頂点データ
+    //共通
+    element.normal = input[0].normal; //法線をコピー
+   
+    //1点目
+    element.svpos = input[0].svpos; //頂点座標をコピー
+    element.uv = input[0].uv; //UVをコピー
+    output.Append(element);
+    //2点目
+    element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0); //頂点座標をコピー
+    element.uv = input[0].uv + float2(1,1); //UVをコピー
+    output.Append(element);
+    //3点目
+    element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0); //頂点座標をコピー
+    element.uv = input[0].uv + float2(1, 0); //UVをコピー
+    output.Append(element);
 }
